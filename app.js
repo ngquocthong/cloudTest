bb0213de2158edccef2dd8a3ab0e4bce1ae726ec
Quoc Thong <39300386+ngquocthong@users.noreply.github.com>
2022-09-19 19:11:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Quoc Thong dep trai nhat Can Tho</h1>');
+    res.end('<h1>Quoc Thong dep trai nhat Can Tho
+          </h1>
+            <img src="./img/thong.png" alt="Nature" width="400" height="700">');
 });
 
 server.listen(port, () => {
